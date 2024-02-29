0b02ca7d350f35d8ac58817fae32d2e8ae160279
RohilPrajapati <rohilprajapati@gmail.com>
2024-02-29 22:30:50 +0545

    inital all files

---
+++ backend/app/Http/Controllers/PostController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $post = Post::all();
+        return response()->json($post);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'status' => 'required',
+        ]);
+        $data = $request->json()->all();
+        $post = new Post();
+        $post['title'] = $data['title'];
+        $post['description'] = $data['description'];
+        $post['status'] = $data['status'];
+        $post->save();
+        return response()->json($post);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $post = Post::findOrFail($id);
+        return response()->json($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'status' => 'required',
+        ]);
+        $data = $request->json()->all();
+        $post = Post::findOrFail($id);
+        $post['title'] = $data['title'];
+        $post['description'] = $data['description'];
+        $post['status'] = $data['status'];
+        $post->save();
+        return response()->json($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return response()->json(["message"=>"Deleted"]);
+    }
+}
